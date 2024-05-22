64b173ba05f21fe5f982646f0efc3ee1b3d50151
mejomejo <1975815008@qq.com>
2024-05-22 12:15:30 +0800

    feat: 抽奖活动库表及持久层对象

---
+++ big-market-infrastructure/src/main/java/cn/dyx/infrastructure/persistent/po/RaffleActivityCount.java
@@ -0,0 +1,49 @@
+package cn.dyx.infrastructure.persistent.po;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @author dyx
+ * @description 抽奖活动次数配置表 持久化对象
+ * @create 2024/5/22 12:07
+ */
+@Data
+public class RaffleActivityCount {
+    /**
+     * 自增ID
+     */
+    private Long id;
+
+    /**
+     * 活动次数编号
+     */
+    private Long activityCountId;
+
+    /**
+     * 总次数
+     */
+    private Integer totalCount;
+
+    /**
+     * 日次数
+     */
+    private Integer dayCount;
+
+    /**
+     * 月次数
+     */
+    private Integer monthCount;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+}
