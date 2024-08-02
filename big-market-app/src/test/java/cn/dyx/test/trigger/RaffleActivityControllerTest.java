1ac4707a26ab90331b8aa36c237739be46b26b37
dyx <1975815008@qq.com>
2024-08-02 11:25:13 +0800

    feat: 积分发奖服务实现.

--- big-market-app/src/test/java/cn/dyx/test/trigger/RaffleActivityControllerTest.java
+++ big-market-app/src/test/java/cn/dyx/test/trigger/RaffleActivityControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * @author dyx
@@ -44,9 +45,24 @@ public class RaffleActivityControllerTest {
     }
 
     @Test
-    public void test_isCalendarSignRebate() {
-        Response<Boolean> response = raffleActivityService.isCalendarSignRebate("xiaofuge");
+    public void test_calendarSignRebate() throws InterruptedException {
+        Response<Boolean> response = raffleActivityService.calendarSignRebate("user001");
         log.info("测试结果：{}", JSON.toJSONString(response));
+        // 让程序挺住方便测试，也可以去掉
+        new CountDownLatch(1).await();
+    }
+
+    @Test
+    public void test_blacklist_draw() throws InterruptedException {
+        ActivityDrawRequestDTO request = new ActivityDrawRequestDTO();
+        request.setActivityId(100301L);
+        request.setUserId("user001");
+        Response<ActivityDrawResponseDTO> response = raffleActivityService.draw(request);
+        log.info("请求参数：{}", JSON.toJSONString(request));
+        log.info("测试结果：{}", JSON.toJSONString(response));
+
+        // 让程序挺住方便测试，也可以去掉
+        new CountDownLatch(1).await();
     }
 
     @Test
